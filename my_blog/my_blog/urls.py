fb50e5fef686215155a3f9445d590229e2e7806b
kenny <1979216613@qq.com>
2017-02-10 14:07:08 +0800

    博客详情页展示

--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -20,6 +20,6 @@ from article import views as article_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', article_views.home, name='home'),
-    url(r'^(?P<my_args>\d+)/$', article_views.detail, name= 'detail'),
+    url(r'^(?P<id>\d+)/$', article_views.detail, name= 'detail'),
     url(r'^test/$', article_views.test, name='test'),
 ]
